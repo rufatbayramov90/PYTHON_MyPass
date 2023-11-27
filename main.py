7cab304e9c4058f45fd7cfd2d1e281da4828ecc4
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-27 16:02:26 +0400

    UI setup

---
+++ main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=60,pady=60)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+
+#Labels
+website_label = Label(text="Website : ")
+website_label.grid(row=1,column=0)
+email_label = Label(text="Email/UserName : ")
+email_label.grid(row=2,column=0)
+password_label = Label(text="Password : ")
+password_label.grid(row=3,column=0)
+
+#Entries
+website_entry = Entry(width=45)
+website_entry.grid(row=1,column=1, columnspan = 2)
+email_entry = Entry(width=45)
+email_entry.grid(row=2,column=1, columnspan = 2)
+password_entry = Entry(width=27)
+password_entry.grid(row=3,column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3,column=2)
+add_button = Button(text="Add", width = 38)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
